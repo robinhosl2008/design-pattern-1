0873c623da834870d67a464053ef8a6e1c717991
Robson Souza Lourenço <robson.lourenco.pacheco@gmail.com>
2023-05-22 11:49:40 -0300

    Adicionando ao .gitignore

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
 use Alura\DesignPattern1\CalculadoraImpostos;
 use Alura\DesignPattern1\Orcamento;
 
-require_once "vendor/autoload.php";
+require_once "./vendor/autoload.php";
 
 $calculadora = new CalculadoraImpostos();
 
 $orcamento = new Orcamento();
 $orcamento->valor = 100;
-var_dump($calculadora); exit();
-echo $calculadora->calcula($orcamento->valor, 'ICMS');
+
+echo $calculadora->calcula($orcamento, 'ICMS');
